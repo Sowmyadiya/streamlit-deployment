e46d69c7cbd1298928d0a8a67abce5188dba50c5
Sowmya Ravichandran <49642360+Sowmyadiya@users.noreply.github.com>
2022-11-18 20:52:52 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@
 # streamlit_app.py
 
 import streamlit as st
-from google.oauth2 import service-acc
+from google.oauth2 import service_account
 from google.cloud import bigquery
 
 # Create API client.
-credentials = service-acc.Credentials.from_service_account_info(
+credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 client = bigquery.Client(credentials=credentials)
